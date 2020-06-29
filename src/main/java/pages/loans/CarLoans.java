962cbd4cda40fba6cd4ffd24df5b2aabfbdb8930
eugene <utyfdfrthby08@gmail.com>
2020-06-29 17:11:18 +0300

    from office (home) add structure + MobilePhoneReplenishmentTest

---
+++ src/main/java/pages/loans/CarLoans.java
@@ -0,0 +1,12 @@
+package pages.loans;
+
+import org.openqa.selenium.WebDriver;
+
+public class CarLoans {
+    WebDriver driver;
+
+    public CarLoans(WebDriver driver) {
+        this.driver = driver;
+    }
+}
+
---
+++ src/main/java/pages/telecomunications/MobilePhoneReplenishmentPage.java
@@ -0,0 +1,27 @@
+package pages.telecomunications;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import pages.base.BasePage;
+
+public class MobilePhoneReplenishmentPage extends BasePage {
+
+    /**
+     * To get driver from BasePage.class
+     *
+     * @param driver
+     */
+    public MobilePhoneReplenishmentPage(WebDriver driver) {
+        super(driver);
+    }
+
+
+    private final By buttonWallet = By.xpath("//div[contains(text(),'My wallet')]");
+
+
+    public MobilePhoneReplenishmentPage selectCardFromWallet() {
+        driver.findElement(buttonWallet).click();
+        return this;
+    }
+
+}
